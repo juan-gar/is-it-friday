5cffec261720cad9b629925325f27929f5a09be2
juanillo <guerrerosagrado@gmail.com>
2018-02-03 18:11:54 +0000

    removed weekdays array

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-
-
 const now = new Date();
 
 const day = now.getDay();
 
-const weekDay = days[day]
-
-console.log(weekDay)
 
 const node = document.createElement('h1')
 const textNodeYes = document.createTextNode('YES IT IS')
 const textNodeNo = document.createTextNode("NO IT'S NOT. START CRYING")
-const textNodeWk = document.createTextNode("No, but it's the weekend!")
+const textNodeWk = document.createTextNode("No, but it's the weekend! Enjoy!")
 
 //
 
 
 function revealDay(){
 
-  if (weekDay == 'Friday'){
+  if (day == 5){
     node.appendChild(textNodeYes);
     document.getElementById("maindiv").appendChild(node)
     
   
-  } else if (weekDay == 'Saturday' || weekDay == 'Sunday') {
+  } else if (day == 6 || day == 7) {
     node.appendChild(textNodeWk);
     document.getElementById("maindiv").appendChild(node)
   } else {
