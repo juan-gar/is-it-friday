dad45f5a5e0aa233efd82e329038b26f2242a484
juanillo <guerrerosagrado@gmail.com>
2018-02-03 16:25:23 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+
+const now = new Date();
+
+const day = now.getDay();
+
+const weekDay = days[day]
+
+console.log(weekDay)
+
+const node = document.createElement('h1')
+const textNodeYes = document.createTextNode('YES IT IS')
+const textNodeNo = document.createTextNode("NO IT'S NOT. START CRYING")
+const textNodeWk = document.createTextNode("No, but it's the weekend!")
+
+//
+
+
+function revealDay(){
+
+  if (weekDay == 'Friday'){
+    node.appendChild(textNodeYes);
+    document.getElementById("maindiv").appendChild(node)
+    
+  
+  } else if (weekDay == 'Saturday' || weekDay == 'Sunday') {
+    node.appendChild(textNodeWk);
+    document.getElementById("maindiv").appendChild(node)
+  } else {
+    node.appendChild(textNodeNo);
+    document.getElementById("maindiv").appendChild(node)
+  }
+}
