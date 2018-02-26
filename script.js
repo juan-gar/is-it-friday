688ecb5454ed075805a7f4d5dabbbab0d9379b1a
juanillo <guerrerosagrado@gmail.com>
2018-02-26 11:00:33 +0000

    Added button to copy gif to clipboard

--- script.js
+++ script.js
@@ -76,9 +76,42 @@ function revealDay(){
     .then((resp) => resp.json())
     .then(function(data){
       const gif = data.data.image_url
-      document.getElementById('giphy').innerHTML = `<center><img src = ${gif} title="GIF via Giphy"></center>`;
+      document.getElementById('giphy').innerHTML = `<center><img id="daGif" src = ${gif} title="GIF via Giphy"></center></br><input id="copyBtn" type="button" value="Copy da Giphy" id="submit">`;
+      document.getElementById('copyBtn').addEventListener("click", function(){
+        copyToClipboard(gif);
+        copyBtn.value = "Da Giphy is copied!"
+      })
+      console.log(gif)
     })
   
-  
+}
+
+
+// Copy the gif to the clipboard for sharing
+
+function copyToClipboard(text) {
+ 
+if (window.clipboardData && window.clipboardData.setData) {
+ // IE specific code path to prevent textarea being shown while dialog is visible.
+ 
+return clipboardData.setData("Text", text); 
+ 
+} 
+ 
+else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
 }
 
